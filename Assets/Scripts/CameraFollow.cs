c759ba9400611715767e5bb4073e9d608dc82307
Dnny <mcmillandanny@gmail.com>
2018-11-19 17:50:09 -0500

    added some sound

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,21 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow: MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
 
-    public GameObject player;
+    public Transform target;
+    public Vector2 targetOffset;
 
-    private Vector3 offset;
+    public Transform topRightBounds;
+    public Transform bottomLeftBounds;
 
-    // Use this for initialization
-    void Start () {
+    public Vector2 offset;
 
-        offset = transform.position - player.transform.position;
-    }
-    
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
+        float xPos = transform.position.x;
+        float yPos = target.position.y + targetOffset.y;
 
-        transform.position = player.transform.position + offset;
+
+        if (target.position.x < bottomLeftBounds.position.x)
+        {
+            xPos += target.position.x - bottomLeftBounds.position.x;
+        }
+        else if (target.position.x > topRightBounds.position.x)
+        {
+            xPos += target.position.x - topRightBounds.position.x;
+        }
+
+        Vector3 newPos = new Vector3(xPos + offset.x, yPos + offset.y, transform.position.z);
+        transform.position = newPos;
+    }
+
+    public void ChangeOffset(Vector2 newOffset)
+    {
+        targetOffset = newOffset;
     }
 }
---
+++ Assets/Scripts/CameraZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZone : MonoBehaviour
+{
+
+    CameraFollow follow;
+
+    public Vector2 zoneOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        follow = Camera.main.GetComponent<CameraFollow>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        follow.ChangeOffset(zoneOffset);
+    }
+
+}
