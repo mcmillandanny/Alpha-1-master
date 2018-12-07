72d4b56ae72b6a2b8afbf486c702f32ae5f472dc
Dnny <mcmillandanny@gmail.com>
2018-12-07 17:19:47 -0500

    fixed some bugs/ upped the UI

---
+++ Assets/Scripts/MoveOnTouch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnTouch : MonoBehaviour {
+
+    [SerializeField] private Vector3 veloity;
+
+    private bool moving;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            moving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
